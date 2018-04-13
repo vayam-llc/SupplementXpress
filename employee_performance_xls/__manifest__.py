8e9700af06469efb8a03124d6cbe7a54394b062b
salman <salmankh334@gmail.com>
2018-04-13 16:32:00 +0530

    Employee Performance

---
+++ employee_performance_xls/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name':'Employee Performance XLS report',
+    'author':"Socius IGB",
+    'version':"1.0",
+    'category':"Sale",
+    'description':"Employee Performance Report",
+    'depends':['account','purchase','sale','product_brand'],
+    'data':['views/employee_report.xml',
+            'views/pl_view.xml'],
+    'application':False,
+    'installable': True,
+
+}
