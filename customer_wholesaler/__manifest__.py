ccf232c6cc7a573b7b24c42b1624a18fcebad47c
salman <salmankh334@gmail.com>
2018-04-24 14:51:51 +0530

    Wholesale customer

---
+++ customer_wholesaler/__manifest__.py
@@ -0,0 +1,16 @@
+
+{
+    'name': 'wholesale customer',
+    'category': 'sale',
+    'version': '1.0',
+    'description': "Set a customer as wholesaler",
+    'depends': ['base','website_sale'],
+    'installable': True,
+    'data': [
+        'views/wholesaler.xml',
+        'views/wholesale_page.xml',
+        'views/templates.xml',
+
+    ],
+    'application': True,
+}
