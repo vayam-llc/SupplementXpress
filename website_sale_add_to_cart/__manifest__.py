29ce2ac41ae27e088e2d6e4147c83303febfb5bd
salman <salmankh334@gmail.com>
2018-04-25 12:33:07 +0530

    Add to cart customization

---
+++ website_sale_add_to_cart/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Quick add items to shopping cart',
+    'version': '1.0.2',
+    'author': 'Socius IGB',
+    'license': 'LGPL-3',
+    'category': 'eCommerce',
+    'website': 'https://sociusigb.com',
+    'depends': ['website_sale'],
+    'images': ['images/shop.png'],
+    'data': [
+        'website_sale_add_to_cart_views.xml',
+    ],
+    'installable': True
+}
