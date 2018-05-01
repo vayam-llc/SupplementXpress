a071e85ccc6e9a0fe80ecf2d70a7461f45b54151
sarga <sargababuc@gmail.com>
2018-04-30 19:28:32 -0700

    version.0.1

--- product_pricelist_sx/models/__init__.py
+++ product_pricelist_sx/models/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-import pricelist
-import partner
-import product
+from . import pricelist
+from . import partner
+from . import product
